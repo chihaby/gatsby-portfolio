0397fc7e49e89c745ed9d302d69b07cb1d7704ea
Rad <chihaby@gmail.com>
2021-12-11 10:23:56 -0600

    edits api url in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `process.env.API_URL || 'http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`job`, `project`],
         //If using single types place them in this array.
