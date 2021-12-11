ed8eefe44f7b191bfcd82cbed1e80b4b8ff2def0
Rad <chihaby@gmail.com>
2021-12-10 21:50:47 -0600

    adds public files

---
+++ public/component---src-pages-404-js-28dbf60983a3948990b6.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var r=a(7294),l=a(5444),n=a(1633);t.default=function(){return r.createElement(r.Fragment,null,r.createElement(n.Z,{title:"Not Found"}),r.createElement("main",{className:"error-page"},r.createElement("div",{className:"error-container"},r.createElement("h1",null,"oops it's a dead end"),r.createElement(l.rU,{to:"/",className:"btn"},"back home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-28dbf60983a3948990b6.js.map
